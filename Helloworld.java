f22c168d0ed5b6e3000d9ce92f96146cb7e668f7
sudhakar <sudhakarsingi161@gmail.com>
2025-02-15 02:22:39 +0000

    Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,21 @@
 public class Helloworld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        System.out.println("Hello, World!");
+
+        // Arithmetic operations
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+
+        // Display results of arithmetic operations
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+        System.out.println("Difference of " + a + " and " + b + " is: " + difference);
+        System.out.println("Product of " + a + " and " + b + " is: " + product);
+        System.out.println("Quotient of " + a + " and " + b + " is: " + quotient);
     }
 }
 
